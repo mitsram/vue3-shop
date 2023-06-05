051ea4bdb17fe3551c41b94ad7b87cd1cd74095c
Mitchell Ramos <mitchell.ramos@aurizon.com.au>
2023-06-05 16:54:45 +1000

    Build html and css

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # vue3-shop
 
+![Shop](./src/assets/images/main_page.png)
+
 This template should help get you started developing with Vue 3 in Vite.
 
 ## Recommended IDE Setup
--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,67 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+  <header class="header">
+      <span class="logo">mitsram</span>
+      <nav class="menu">
+        <ul>          
+          <li><a href="#">Products</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
       </nav>
-    </div>
+      <ul class="profile">
+        <li><a class="cart" href="#"><img src="./assets/shopping_cart.svg" alt="cart"><label>(1)</label></a></li>
+        <li><a class="account" href="#"><img src="./assets/account_circle.svg" alt="account"></a></li>
+      </ul>
   </header>
+  
+  <main class="content">
+    <section class="section">
+      <div class="container">
+        <div class="search-bar">
+          <input type="text" placeholder="Search something...">
+          <button type="submit"><img src="./assets/search.svg" alt=""></button>
+        </div>
+        <h2>Shop Now</h2>        
+        <div class="products">
+          <div class="product-card">
+            <img src="./assets/images/watch.jpeg" alt="Product 1">
+            <h3>Product 1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <button class="btn">Add to Cart</button>
+          </div>
+          <div class="product-card">
+            <img src="./assets/images/camera.jpeg" alt="Product 2">
+            <h3>Product 2</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <button class="btn">Add to Cart</button>
+          </div>
+          <div class="product-card">
+            <img src="./assets/images/headset.jpeg" alt="Product 3">
+            <h3>Product 3</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <button class="btn">Add to Cart</button>
+          </div>
+          <div class="product-card">
+            <img src="./assets/images/herbal.jpeg" alt="Product 4">
+            <h3>Product 4</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <button class="btn">Add to Cart</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 
-  <RouterView />
+  <footer class="footer">
+    <div class="container">
+      <p>&copy; 2023 mitsram. All rights reserved.</p>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
